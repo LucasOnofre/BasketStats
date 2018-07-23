3c287f440be8c264cec085e172d28d817209c025
Lucas Onofre Calisto Rosa <lucas-onofre@hotmail.com>
2018-07-23 16:17:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/nbastats/model/Players.java
@@ -0,0 +1,24 @@
+package com.example.lucas.nbastats.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Players {
+
+    @SerializedName("rookieYear")
+    private Integer rookieYear;
+
+    @SerializedName("lastName")
+    private Integer albumId;
+
+    @SerializedName("firstName")
+    private Integer firstName;
+
+    @SerializedName("birthDate")
+    private Integer birthDate;
+
+    @SerializedName("status")
+    private Integer status;
+
+    @SerializedName("team")
+    private Integer team;
+}
